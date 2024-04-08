59844c0ac1af0f16079a8ffd9558e812e80fafaf
NailahBala <160489283+NailahBala@users.noreply.github.com>
2024-04-08 14:14:57 +0200

    creation de la classe TagAlongBike de l'exercice 1

---
+++ src/main/java/fr/polytech/sim/cycling/TagAlongBike.java
@@ -0,0 +1,15 @@
+package main.java.fr.polytech.sim.cycling;
+
+import fr.polytech.sim.cycling.SimpleBike;
+import fr.polytech.sim.transport.Wheel;
+
+public class TagAlongBike extends SimpleBike{
+
+    /**
+     * Constructor.
+     */
+    public TagAlongBike() {
+        components.add(new SimpleBike(this));
+    }
+
+}
