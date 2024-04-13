a570e09591e5d8aafac02d31a36f1cb8e57235a0
Nailah Bala <na'ilah.bala@etu.univ-lyon1.fr>
2024-04-13 16:59:27 +0200

    modification de la méthode run : appel de la fonction injectAll pour itérer sur toutes les instances de la classe Bike

--- src/main/java/fr/polytech/sim/cycling/BikeSimulator.java
+++ src/main/java/fr/polytech/sim/cycling/BikeSimulator.java
@@ -5,6 +5,8 @@ import fr.polytech.sim.log.Logger;
 import fr.polytech.sim.log.LoggerCreator;
 import fr.polytech.sim.utils.Context;
 
+import java.util.Iterator;
+
 /**
  * Bike simulation.
  */
@@ -12,13 +14,16 @@ public class BikeSimulator implements Simulation {
     private final Logger logger = LoggerCreator.factoryMethod(this.getClass().toString());
 
     public void run() {
+        this.logger.log("[SIMULATION] Lancement de la simulation avec les différentes instances de vélos disponibles : ");
 
-        // Injection d'une instance de Bike par le contexte applicatif
-        Context context = new Context();
-        Bike bike = context.inject(Bike.class);
-
-        this.logger.log("\n[SIMULATION] Lancement de la simulation avec une instance de " + bike.getClass() + " : ");
-        this.logger.log("[SIMULATION] Bike's speed %.2f Km/h.", bike.getVelocity());
-        this.logger.log("[SIMULATION] Bike's mass %.2f Kg.", bike.getMass());
+        // Récupération de toutes les instances de Bike
+        Iterator<Bike> bikesIterator = Context.injectAll(Bike.class);
+        while (bikesIterator.hasNext()) {
+            Bike bike = bikesIterator.next();
+            this.logger.log("[SIMULATION] Instance de " + bike.getClass() + " : ");
+            this.logger.log("[SIMULATION] Bike's speed %.2f Km/h.", bike.getVelocity());
+            this.logger.log("[SIMULATION] Bike's mass %.2f Kg.", bike.getMass());
+            this.logger.log(" ");
+        }
     }
 }
