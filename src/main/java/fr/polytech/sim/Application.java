a429a1067c25dcd25017d565b936496758c395ab
Nailah Bala <na'ilah.bala@etu.univ-lyon1.fr>
2024-04-13 16:58:43 +0200

    ajout d'un test dans le main pour une instance de la classe SimpleBike

--- src/main/java/fr/polytech/sim/Application.java
+++ src/main/java/fr/polytech/sim/Application.java
@@ -1,6 +1,7 @@
 package fr.polytech.sim;
 
 import fr.polytech.sim.cycling.BikeSimulator;
+import fr.polytech.sim.cycling.SimpleBike;
 import fr.polytech.sim.cycling.TagAlongBike;
 
 /**
@@ -19,7 +20,12 @@ public class Application {
 
         System.out.println("Test des fonctions getVelocity et getMass de la classe TagAlongBike :");
         TagAlongBike tagAlongBike = new TagAlongBike();
-        System.out.println("Velocité de tagAlongBike : " + tagAlongBike.getVelocity());    
-        System.out.println("Masse de tagAlongBike : " + tagAlongBike.getMass());    
+        System.out.println("Velocité de tagAlongBike : " + tagAlongBike.getVelocity());
+        System.out.println("Masse de tagAlongBike : " + tagAlongBike.getMass());
+
+        System.out.println("Test des fonctions getVelocity et getMass de la classe SimpleBike :");
+        SimpleBike simpleBike = new SimpleBike();
+        System.out.println("Velocité de simpleBike : " + simpleBike.getVelocity());
+        System.out.println("Masse de simpleBike : " + simpleBike.getMass());
     }
 }
