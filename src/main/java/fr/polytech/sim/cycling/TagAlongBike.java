39bf27fcc0ed7db3f1fcecd07b7dc887f8f8b73f
Fatou <ft.magassa@gmail.com>
2024-04-14 16:11:16 +0200

    Correction de l'extend de TagAlongBike (hérite de SimpleBike qui hérite de Bike) pour respecter le pattern Composite

--- src/main/java/fr/polytech/sim/cycling/TagAlongBike.java
+++ src/main/java/fr/polytech/sim/cycling/TagAlongBike.java
@@ -1,9 +1,6 @@
 package fr.polytech.sim.cycling;
 
-import fr.polytech.sim.cycling.SimpleBike;
-import fr.polytech.sim.transport.Wheel;
-
-public class TagAlongBike extends Bike{
+public class TagAlongBike extends SimpleBike {
 
     /**
      * Constructor.
